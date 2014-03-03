a1d07e6b471e93c7bbd5b71776a35d7fda17130b
zeitgeist7 <zeitgeist7@gmail.com>
2014-03-03 01:27:07 +0100

    Create a scene of type HelloScene and present it in the VC’s view
    #happy

--- SpriteWalkthrough/SpriteViewController.m
+++ SpriteWalkthrough/SpriteViewController.m
@@ -8,6 +8,7 @@
 
 @import SpriteKit;
 #import "SpriteViewController.h"
+#import "HelloScene.h"
 
 @interface SpriteViewController ()
 
@@ -22,6 +23,16 @@
     spriteView.showsDrawCount = YES;    // fewer the better
     spriteView.showsNodeCount = YES;    // yeah whatever
     spriteView.showsFPS = YES;  // should try to maintain it to a constant frame rate
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    HelloScene *hello = [[HelloScene alloc] initWithSize:CGSizeMake(768.0, 1024.0)];
+    SKView *spriteView = (SKView *)self.view;
+    [spriteView presentScene:hello];
+    
 }
 
 @end
