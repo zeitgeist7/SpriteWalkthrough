9994bd5ed3c5b8b48d0826c3d290b86464d73b9a
zeitgeist7 <zeitgeist7@gmail.com>
2014-03-03 01:21:08 +0100

    Add new scene subclass Put controller(s) in its own group
    #tidy #project

---
+++ SpriteWalkthrough/View (& scenes)/HelloScene.h
@@ -0,0 +1,13 @@
+//
+//  HelloScene.h
+//  SpriteWalkthrough
+//
+//  Created by Rajiv Jhoomuck on 03/03/14.
+//  Copyright (c) 2014 Shunyaek. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface HelloScene : SKScene
+
+@end
---
+++ SpriteWalkthrough/View (& scenes)/HelloScene.m
@@ -0,0 +1,13 @@
+//
+//  HelloScene.m
+//  SpriteWalkthrough
+//
+//  Created by Rajiv Jhoomuck on 03/03/14.
+//  Copyright (c) 2014 Shunyaek. All rights reserved.
+//
+
+#import "HelloScene.h"
+
+@implementation HelloScene
+
+@end
