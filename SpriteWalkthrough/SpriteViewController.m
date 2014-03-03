b334ff13df848edd0c9105cbd80e860cfe612b4a
zeitgeist7 <zeitgeist7@gmail.com>
2014-03-03 01:03:03 +0100

    Change the class of the view in the VC to be of class SKView
    - Clean up app delegate and the view controller
    
    #initial #commit

--- SpriteWalkthrough/SpriteViewController.m
+++ SpriteWalkthrough/SpriteViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Shunyaek. All rights reserved.
 //
 
+@import SpriteKit;
 #import "SpriteViewController.h"
 
 @interface SpriteViewController ()
@@ -17,13 +18,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
